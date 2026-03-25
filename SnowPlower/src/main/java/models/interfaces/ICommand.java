c61b19b9774a9b5e6008ecf476df69270d5ca6fa
Izer Botond <62500202+BotIzer@users.noreply.github.com>
2026-03-25 19:14:15 +0100

    Console, ICommand implementation for skeleton

--- SnowPlower/src/main/java/models/interfaces/ICommand.java
+++ SnowPlower/src/main/java/models/interfaces/ICommand.java
@@ -1,5 +1,16 @@
 package main.java.models.interfaces;
 
-public class ICommand {
-    
+import main.java.models.interfaces.*;
+import main.java.models.objects.*;
+
+public interface ICommand {
+   boolean start();
+   boolean end();
+   boolean saveState();
+   boolean setRoute(IVehicle vehicle);
+   boolean selectPlower(SnowPlower sp);
+   boolean buyEquipment();
+   boolean changeEquipment(ICleaning newEq);
+   String printVehicles();
+   String printInventory();
 }
--- SnowPlower/src/main/java/models/objects/Console.java
+++ SnowPlower/src/main/java/models/objects/Console.java
@@ -1,5 +1,82 @@
 package main.java.models.objects;
+import main.java.models.interfaces.ICommand;
+import java.util.logging.Logger;
+import main.java.models.interfaces.*;
+import main.java.models.objects.*;
 
-public class Console {
-    
+public class Console implements ICommand {
+    static Logger logger = Logger.getLogger(Console.class.getName());
+    private Player player;
+    private Map map;
+    private IVehicle selectedPlower;
+
+    public void input(String cmd){
+        logger.info("-> Console.input()");
+        logger.info("<- Console.input()");
+    }
+
+    @Override
+    public boolean start() {
+        logger.info("-> Console.start()");
+        logger.info("<- Console.start():true");
+        return true;
+    }
+
+    @Override
+    public boolean end() {
+        logger.info("-> Console.end()");
+        logger.info("<- Console.end():true");
+        return true;
+    }
+
+    @Override
+    public boolean saveState() {
+        logger.info("-> Console.saveState()");
+        logger.info("<- Console.saveState():true");
+        return true;
+    }
+
+    @Override
+    public boolean setRoute(IVehicle vehicle) {
+        logger.info("-> Console.setRoute(vehicle)");
+        logger.info("<- Console.setRoute(vehicle):true");
+        return true;
+    }
+
+    @Override
+    public boolean selectPlower(SnowPlower sp) {
+        logger.info("-> Console.selectPlower(sp)");
+        logger.info("<- Console.selectPlower(sp):true");
+        return true;
+    }
+
+    @Override
+    public boolean buyEquipment() {
+        logger.info("-> Console.buyEquipment()");
+        logger.info("<- Console.buyEquipment():true");
+        return true;
+    }
+
+    @Override
+    public boolean changeEquipment(ICleaning newEq) {
+        logger.info("-> Console.changeEquipment(newEq)");
+        logger.info("<- Console.changeEquipment(newEq):true");
+        return true;
+    }
+
+    @Override
+    public String printVehicles() {
+        logger.info("-> Console.printVehicles()");
+        logger.info("<- Console.printVehicles():String");
+        return "";
+    }
+
+    @Override
+    public String printInventory() {
+        logger.info("-> Console.printInventory()");
+        logger.info("<- Console.printInventory():String");
+        return "";
+    }
+
+
 }
