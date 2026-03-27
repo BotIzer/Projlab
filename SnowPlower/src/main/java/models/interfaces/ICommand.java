8610a1742cf38e2a4818b2b29a35372746d34687
Izer Botond <62500202+BotIzer@users.noreply.github.com>
2026-03-27 11:53:26 +0100

    Add ICommand interface implementation

--- SnowPlower/src/main/java/models/interfaces/ICommand.java
+++ SnowPlower/src/main/java/models/interfaces/ICommand.java
@@ -1,5 +1,13 @@
 package main.java.models.interfaces;
 
-public class ICommand {
-    
+public interface ICommand {
+   boolean start();
+   boolean end();
+   boolean saveState();
+   boolean setRoute(IVehicle vehicle);
+   boolean selectPlower(SnowPlower sp);
+   boolean buyEquipment();
+   boolean changeEquipment(ICleaning newEq);
+   String printVehicles();
+   String printInventory();
 }
