eed705a95706623e246376ff4c61047e34e8c225
Izer Botond <62500202+BotIzer@users.noreply.github.com>
2026-03-27 11:51:15 +0100

    Resolve merge conflict

--- SnowPlower/src/main/java/models/interfaces/ICommand.java
+++ SnowPlower/src/main/java/models/interfaces/ICommand.java
@@ -1,16 +1,5 @@
 package main.java.models.interfaces;
 
-import main.java.models.interfaces.*;
-import main.java.models.objects.*;
-
-public interface ICommand {
-   boolean start();
-   boolean end();
-   boolean saveState();
-   boolean setRoute(IVehicle vehicle);
-   boolean selectPlower(SnowPlower sp);
-   boolean buyEquipment();
-   boolean changeEquipment(ICleaning newEq);
-   String printVehicles();
-   String printInventory();
+public class ICommand {
+    
 }
